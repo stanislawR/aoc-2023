f75a601d6648fb7da4795a44cca6caf9a3aa1057
Stanislaw Rojek <stas.rojek132@gmail.com>
2023-12-11 12:12:24 +0100

    Day 5 part 2

--- day05/index.js
+++ day05/index.js
@@ -33,8 +33,6 @@ function resolveMap(seeds, mapData) {
     .forEach((seed) => {
       seed[to] = seed[from];
     });
-
-  return;
 }
 
 function resolveSeeds(seeds, data) {
@@ -47,22 +45,96 @@ function resolveSeeds(seeds, data) {
   }
 }
 
-function part1(seeds) {
-  return seeds.map(({ location }) => location).sort((a, b) => a - b)[0];
-}
-
-function part2(data) {}
-
-function process(data) {
+function part1(data) {
   const seeds = readSeeds(data[0]);
   data.splice(0, 2);
   resolveSeeds(seeds, data);
 
+  return seeds.map(({ location }) => location).sort((a, b) => a - b)[0];
+}
+
+function readRanges(line) {
+  return line
+    .split(': ')[1]
+    .split(' ')
+    .reduce((ranges, value, index, array) => {
+      if (index % 2 === 0) {
+        ranges.push({
+          from: +array[index],
+          to: +array[index] + +array[index + 1] - 1,
+        });
+      }
+      return ranges;
+    }, []);
+}
+
+function resolveMapPart2(ranges, mapData) {
+  mapData.shift();
+
+  ranges.forEach((range) => {
+    range.changed = false;
+  });
+
+  mapData
+    .filter((line) => line != '')
+    .forEach((md) => {
+      const description = md.split(' '); // [0] destination range start, [1] source range start, [2] range length
+      const ds = +description[0];
+      const ss = +description[1];
+      const sf = ss + +description[2] - 1;
+
+      for (let i = 0; i < ranges.length; i++) {
+        const { from, to, changed } = ranges[i];
+
+        if (changed) {
+          continue;
+        }
+
+        const union = { from: Math.max(from, ss), to: Math.min(to, sf) };
+
+        if (union.from > union.to) {
+          continue;
+        }
+
+        if (from !== union.from) {
+          ranges.push({ from, to: union.from - 1 });
+        }
+
+        if (to !== union.to) {
+          ranges.push({ from: union.to + 1, to });
+        }
+
+        ranges[i] = {
+          from: union.from - ss + ds,
+          to: union.to - ss + ds,
+          changed: true,
+        };
+      }
+      return;
+    });
+}
+
+function part2(data) {
+  const ranges = readRanges(data[0]);
+  data.splice(0, 2);
+
+  while (data.length) {
+    const mapSize = data.findIndex((line) => line === '');
+    resolveMapPart2(
+      ranges,
+      data.splice(0, mapSize !== -1 ? mapSize + 1 : data.length)
+    );
+  }
+
+  return ranges.map(({ from }) => from).sort((a, b) => a - b)[0];
+}
+
+function process(data) {
   console.log('--- Day 5: If You Give A Seed A Fertilizer ---');
-  console.log(part1(seeds));
+  console.log(part1([...data]));
 
   console.log('--- Part Two ---');
-  console.log(part2(data));
+  console.log(part2([...data]));
 }
 
 fs.readFile('day05/input.txt', 'utf8', (_, data) => {
